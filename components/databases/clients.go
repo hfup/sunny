45af776e110301ee73e38861bebacf57ac9ecb43
huangfei <huangfeigo@qq.com>
2025-06-30 21:33:16 +0800

    updated

---
+++ components/databases/clients.go
@@ -0,0 +1,41 @@
+package databases
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"time"
+)
+
+
+func MysqlConnect(dataInfo *DBConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local",
+		dataInfo.DatabaseInfo.User, dataInfo.DatabaseInfo.Password, dataInfo.DatabaseInfo.Host, dataInfo.DatabaseInfo.Port, dataInfo.DatabaseInfo.DbName, dataInfo.DatabaseInfo.Charset)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if dataInfo.MaxIdelConns > 0 {
+		sqlDB, err := db.DB()
+		if err != nil {
+			return nil, err
+		}
+		sqlDB.SetMaxIdleConns(dataInfo.MaxIdelConns)
+	}
+	if dataInfo.MaxOpenConns > 0 {
+		sqlDB, err := db.DB()
+		if err != nil {
+			return nil, err
+		}
+		sqlDB.SetMaxOpenConns(dataInfo.MaxOpenConns)
+	}
+	if dataInfo.MaxLifetime > 0 {
+		sqlDB, err := db.DB()
+		if err != nil {
+			return nil, err
+		}
+		sqlDB.SetConnMaxLifetime(time.Duration(dataInfo.MaxLifetime) * time.Second)
+	}
+	return db, nil
+}
--- components/databases/database.go
+++ components/databases/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gorm.io/gorm"
 	"github.com/hfup/sunny"
+	"errors"
 )
 
 
@@ -18,8 +19,16 @@ type DatabaseInfo struct {
 	Charset  string `yaml:"charset" json:"charset"` // 字符集
 }
 
+type DBConfig struct {
+	DatabaseInfo *DatabaseInfo
+	DbId         string `yaml:"db_id" json:"db_id"` // 数据库id 唯一
+	MaxIdelConns int `yaml:"max_idel_conns" json:"max_idel_conns"` // 最大空闲连接数
+	MaxOpenConns int `yaml:"max_open_conns" json:"max_open_conns"` // 最大打开连接数
+	MaxLifetime  int `yaml:"max_lifetime" json:"max_lifetime"` // 连接最大生命周期
+}
+
 type DBRouterFunc func(key string) (*gorm.DB, error)
-type DBInitHandler func(ctx context.Context) ([]*gorm.DB, error) // 数据库初始化
+type DBInitHandler func(ctx context.Context,opt any) ([]*DBConfig, error) // 数据库初始化
 
 
 // 数据库管理器接口
@@ -32,19 +41,46 @@ type DatabaseMangerInf interface {
 type DatabaseManager struct {
 	initHandler DBInitHandler
 	dbRouterFunc DBRouterFunc
+
+	dbMap map[string]*gorm.DB
 }
+
 
 // 启动数据库管理器
 func (d *DatabaseManager) Start(ctx context.Context,args any,resultChan chan<- sunny.Result) {
-	dbs,err := d.initHandler(ctx)
+	dbConfigs,err := d.initHandler(ctx,args)
 	if err != nil {
 		resultChan <- sunny.Result{
 			Code: 1,
 			Msg: "database manager start error: " + err.Error(),
 		}
+		return
 	}
-	logrus.Infof("database manager start success, db count: %d", len(dbs))
+	for _,dbConfig := range dbConfigs{
+		db,err := MysqlConnect(dbConfig)
+		if err != nil {
+			resultChan <- sunny.Result{
+				Code: 1,
+				Msg: "database manager start error: " + err.Error(),
+			}
+		}
+		d.dbMap[dbConfig.DbId] = db
+	}
 }
+
+
+// 根据key获取数据库
+func (d *DatabaseManager) GetDBFromKey(key string) (*gorm.DB, error) {
+	dbKey,ok := d.dbMap[key]
+	if !ok {
+		return nil, errors.New("database not found")
+	}
+	return dbKey, nil
+}
+
+
+
+
 
 
 
