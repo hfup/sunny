fb32950ceec706b2fff4ceca1d863dc1f9718596
huangfei <huangfeigo@qq.com>
2025-07-14 18:11:56 +0800

    updated

--- components/storages/storage.go
+++ components/storages/storage.go
@@ -17,3 +17,29 @@ type StorageMangerInf interface {
 	GetStorage(storageType string) StorageInf
 	RemoveStorage(storageType string) error
 }
+
+
+type StorageManger struct {
+	storages map[string]StorageInf
+}
+
+func NewStorageManger() *StorageManger {
+	return &StorageManger{
+		storages: make(map[string]StorageInf),
+	}
+}
+
+func (s *StorageManger) AddStorage(storage StorageInf) error {
+	s.storages[storage.GetType()] = storage
+	return nil
+}
+
+func (s *StorageManger) GetStorage(storageType string) StorageInf {
+	return s.storages[storageType]
+}
+
+func (s *StorageManger) RemoveStorage(storageType string) error {
+	delete(s.storages, storageType)
+	return nil
+}
+
